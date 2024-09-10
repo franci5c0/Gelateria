59446177cae712959df485c504b3dbecc76e94fe
franci5c0 <151041099+franci5c0@users.noreply.github.com>
2024-09-10 13:03:10 -0300

    alterar amanhã

--- controllers/pedidosController.js
+++ controllers/pedidosController.js
@@ -3,13 +3,13 @@ const db = require('../config/db'); //importa a conexão com o banco de dados
 
 //SABORES//
 
-//função para obter todos os pedidos
+//função para obter todos os sabores
 const getAllSabores = (req, res) => {
   db.query('SELECT * FROM sabores', (err, results) => {
 
       if (err) {
-          console.error('Erro ao obter os pedidos')
-          res.status(500).send('Erro ao obter os pedidos');
+          console.error('Erro ao obter os sabores')
+          res.status(500).send('Erro ao obter os sabores');
           return;
       }
       res.json(results);
